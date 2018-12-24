0bd6230c379922663a68c92012bf818b06fcadbc
Suyash458 <suyash.behera458@gmail.com>
2018-12-24 20:18:19 +0530

    add q3 solution. add .gitignore

--- q3.py
+++ q3.py
@@ -1,17 +1,28 @@
-from itertools import combinations
+from collections import Counter
 
 def parse_input():
-    return [{
-        'offset_y': int(_.split('@')[1].split(':')[0].split(',')[0].strip()),
-        'offset_x': int(_.split('@')[1].split(':')[0].split(',')[1].strip()),
-        'y': int(_.split('@')[1].split(':')[1].split('x')[0].strip()),
-        'x': int(_.split('@')[1].split(':')[1].split('x')[1].strip()),
-    } for _ in open('q3.txt', 'r').read().splitlines()]
+	return [{
+		'id': int(_.split('@')[0][1::].strip()),
+		'offset_y': int(_.split('@')[1].split(':')[0].split(',')[0].strip()),
+		'offset_x': int(_.split('@')[1].split(':')[0].split(',')[1].strip()),
+		'y': int(_.split('@')[1].split(':')[1].split('x')[0].strip()),
+		'x': int(_.split('@')[1].split(':')[1].split('x')[1].strip()),
+	} for _ in open('q3.txt', 'r').read().splitlines()]
+
 
 def solve():
-    rects = parse_input()
-    overlaps = []
-    id = 0
-    for (a, b) in combinations(rects, 2):
-        pass
+	rects = parse_input()
+	mat = Counter()
+	claim_ids = []
+	for rect in rects:
+		for x in range(rect['offset_x'], rect['offset_x'] + rect['x']):
+			for y in range(rect['offset_y'], rect['offset_y'] + rect['y']):
+				mat[(x, y)] += 1
+	for rect in rects:
+		if all((mat[(x, y)] == 1 for x in range(rect['offset_x'], rect['offset_x'] + rect['x']) for y in range(rect['offset_y'], rect['offset_y'] + rect['y']))):
+			claim_ids.append(rect['id'])
+	overlap = sum(map(lambda x: x > 1, mat.values()))
+	print(overlap)
+	print(claim_ids)
 
+solve()
